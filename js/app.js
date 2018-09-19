cadfc01a102c0d133c30ee8368368bc6cb95aba5
jarrodyellets <jarrod524@gmail.com>
2018-09-19 23:19:59 +0200

    Fixed nav scroll highlight

--- js/app.js
+++ js/app.js
@@ -1,21 +1,25 @@
 $(document).ready(function(){
 
-  $('.navLinks a').on('click', function(event){
-    $(this).parent().find('a').removeClass('linksActive');
-    $(this).addClass('linksActive');
-  })
+  // $('.navLinks a').on('click', function(event){
+  // //   $(this).parent().find('a').removeClass('linksActive');
+  // //   $(this).addClass('linksActive');
+  // // })
 
-  $(window).on('scroll', function(){
-    console.log($('.about').offset().top);
-    console.log($(window).scrollTop());
-    $('.target').each(function(){
-      if($(window).scrollTop() >= $(this).offset().top){
-        let id = $(this).attr('id');
-        $('.navLinks a').removeClass('linksActive');
-        $('#' + id + 'Link').addClass('linksActive');
-      }
-    });
-  });
+  // // $(window).on('scroll', function(){
+  // //   console.log($('.about').offset().top);
+  // //   console.log($(window).scrollTop());
+  // //   $('.target').each(function(){
+  // //     if($(window).scrollTop() >= $(this).offset().top){
+  // //       let id = $(this).attr('id');
+  // //       $('.navLinks a').removeClass('linksScroll');
+  // //       $('.navLinks a').removeClass('linksActive');
+  // //       $('#' + id + 'Link').addClass('linksScroll');
+  // //       $('#' + id + 'Link').addClass('linksActive');
+  // //     }
+  // //   });
+  // // });
+
+  $('.navigation').scrollspy({animate: true});
 
   $('.brand').click(function(){
     scroll_to('.wrapper');
