fe75544137939ae4716ebf6a0a206a753595c03c
jarrodyellets <jarrod524@gmail.com>
2018-09-19 10:33:04 +0200

    Nav highlight on scroll

--- js/app.js
+++ js/app.js
@@ -1,12 +1,34 @@
 $(document).ready(function(){
 
+  $('.navLinks a').on('click', function(event){
+    $(this).parent().find('a').removeClass('linksActive');
+    $(this).addClass('linksActive');
+  })
+
+  $(window).on('scroll', function(){
+    console.log($('.about').offset().top);
+    console.log($(window).scrollTop());
+    $('.target').each(function(){
+      if($(window).scrollTop() >= $(this).offset().top){
+        let id = $(this).attr('id');
+        $('.navLinks a').removeClass('linksActive');
+        $('#' + id + 'Link').addClass('linksActive');
+      }
+    });
+  });
+
   $('.brand').click(function(){
     scroll_to('.wrapper');
+    $('.navLinks a').parent().find('a').removeClass('linksActive');
   });
 
   $('#aboutLink').click(function(){
     scroll_to('#about');
   });
+
+  $('#menuLink').click(function(){
+    scroll_to('#menu');
+  })
 
   $(".scrollDown").click(function(){
     scroll_to('#about');
