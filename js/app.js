06121913087a1f57c91b0c9c77c18352e3c2ef02
jarrodyellets <jarrod524@gmail.com>
2018-10-01 09:30:49 +0200

    Added map to contact

--- js/app.js
+++ js/app.js
@@ -63,6 +63,10 @@ $(document).ready(function(){
     
   })
 
+  $('.mapBefore').mouseenter(function(){
+    $('.mapAfter').fadeIn();
+  })
+
   function scroll_to(div){
     $('html, body').animate({
       scrollTop: $(div).offset().top + 20
